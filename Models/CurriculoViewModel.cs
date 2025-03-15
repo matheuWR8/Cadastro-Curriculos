b093f7ef2173f72111829ee28d20e3ab76f8cc7d
matheuwr8 <matheusswr8@gmail.com>
2025-03-15 12:49:40 -0300

    Criação da classe de Currículo

---
+++ Models/CurriculoViewModel.cs
@@ -0,0 +1,15 @@
+namespace Curriculos.Models
+{
+    public class CurriculoViewModel
+    {
+        public String Nome {get; set;}
+        public String Cpf {get; set;}
+        public String Endereco {get; set;}
+        public String Telefone {get; set;}
+        public String Email {get; set;}
+        public double Salario {get; set;}
+        public String Cargo {get; set;}
+
+        // ToDo: formações, exp. profissionais e idiomas
+    }
+}
